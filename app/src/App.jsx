4bb9b9329794b5452d5a973db5cafa87fb4b83bc
Kevin <kevintang1509@gmail.com>
2024-09-26 13:13:00 -0400

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import {MainContainer, ChatContainer, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
+import {MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
 
 
 function App() {
