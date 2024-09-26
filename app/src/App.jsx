9cecdd95e088e31c99a61a2c054cfabcc0552b14
Kevin <kevintang1509@gmail.com>
2024-09-25 20:03:57 -0400

    not working, nothing renders

---
+++ app/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {MainContainer, ChatContainer, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
+
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT! How are you today?",
+      sender: "ChatGPT"
+    }
+  ])
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage]; //old messages + new message
+
+    //Update the messages state
+    setMessages(newMessages);
+
+
+    //precess message to chatGPT (send and see response)
+  }
+
+  return (
+    <div className="App">
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type messag here" onSend={handleSend}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
+
+export default App
